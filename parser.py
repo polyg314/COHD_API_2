6b414edcca07c3ad9062191b28f540c32dc26976
Paul Gaudin <paulgaudin314@gmail.com>
2020-08-26 22:19:12 -0700

    make sure there are no repeats

--- parser.py
+++ parser.py
@@ -20,14 +20,19 @@ def load_annotations(data_folder):
     for x in xref_data:
         xref_data_dict[x["_id"]] = x
 
+
+    lase_id = '';
+    current_id = '';
     paired_concepts_table_total = pd.read_csv(paired_concept_url, chunksize=CHUNK_SIZE)  
     for chunk in paired_concepts_table_total:
         paired_concepts_table = chunk
         for i,j in paired_concepts_table.iterrows():
+        	last_id = current_id
             current_id_1 = str(int(j["concept_id_1"]))
             current_id_2 = str(int(j["concept_id_2"]))
+            current_id = current_id_1 + "-" + current_id_2
             current_dict = {
-              "_id": current_id_1 + "-" + current_id_2,
+              "_id": current_id,
               "concept1": {
                  "omop": current_id_1,
                  "xrefs": xref_data_dict[current_id_1]["xrefs"]
@@ -49,6 +54,7 @@ def load_annotations(data_folder):
                 }
               ]
             }
-#             print(current_dict)
-            yield(current_dict)
+            if(current_id != last_id): 
+            	yield(current_dict)
+
             
