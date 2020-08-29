7035bfa74bfd6b2efa6acaac8bfb81c598c8f40f
Paul Gaudin <paulgaudin314@gmail.com>
2020-08-29 11:05:02 -0700

    new upload for new sorting

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import os
 
-PAIRED_CONCEPT_FILE_NAME = 'paired_concept_counts_associations.csv'
+PAIRED_CONCEPT_FILE_NAME = 'paired_concept_counts_associations_sorted_cid1_cid2.csv'
 CONCEPT_XREF_FILE_NAME = 'concept_xref.json'
 CHUNK_SIZE = 1000000
 MAX_COMBOS = 100
@@ -20,12 +20,9 @@ def load_annotations(data_folder):
     for x in xref_data:
         xref_data_dict[x["_id"]] = x
 
-    # last_id_1 = ''
+    last_id_1 = ''
     current_id_1 = ''
-    ids_included = []
-    # yield_result = True
-
-    current_results = []
+    first_done = False
 
     paired_concepts_table_total = pd.read_csv(paired_concept_url, chunksize=CHUNK_SIZE)  
 
@@ -38,51 +35,19 @@ def load_annotations(data_folder):
             current_id_2 = str(int(j["concept_id_2"]))
             current_id = current_id_1 + "-" + current_id_2
             if(last_id_1 == current_id_1):
-                if(current_id in ids_included):
-                    for i in range(0,len(current_results)):
-                        if(current_results[i]["_id"] == current_id):
-                            current_results[i]["results"].append({
-                                "concept_count": int(j["concept_count"]),
-                                "concept_prevalence": j["concept_prevalence"],
-                                "dataset_id": j["dataset_id"],
-                                "chi_square_t": j["chi_square_t"],
-                                "chi_square_p": j["chi_square_p"],
-                                "ln_ratio": j["ln_ratio"],
-                                "rel_freq_1": j["rel_freq_1"],
-                                "rel_freq_2":j["rel_freq_2"]
-                            })
-                else:
-                    ids_included.append(current_id)
-                    current_dict = {
-                      "_id": current_id,
-                      "concept1": {
-                         "omop": current_id_1,
-                         "xrefs": xref_data_dict[current_id_1]["xrefs"]
-                      },
-                      "concept2": {
-                         "omop": current_id_2,
-                         "xrefs": xref_data_dict[current_id_2]["xrefs"]
-                      },
-                      "results": [
-                        {
-                            "concept_count": int(j["concept_count"]),
-                            "concept_prevalence": j["concept_prevalence"],
-                            "dataset_id": j["dataset_id"],
-                            "chi_square_t": j["chi_square_t"],
-                            "chi_square_p": j["chi_square_p"],
-                            "ln_ratio": j["ln_ratio"],
-                            "rel_freq_1": j["rel_freq_1"],
-                            "rel_freq_2":j["rel_freq_2"]
-                        }
-                      ]
-                    }
-                    current_results.append(current_dict)
+                current_dict["results"].append({
+                    "concept_count": int(j["concept_count"]),
+                    "concept_prevalence": j["concept_prevalence"],
+                    "dataset_id": j["dataset_id"],
+                    "chi_square_t": j["chi_square_t"],
+                    "chi_square_p": j["chi_square_p"],
+                    "ln_ratio": j["ln_ratio"],
+                    "rel_freq_1": j["rel_freq_1"],
+                    "rel_freq_2":j["rel_freq_2"]
+                })
             else:
-                id_2_matches = [];
-                
-                for x in current_results:
-                   yield(x)
-
+                if(first_done):
+                    yield(current_dict)
                 current_dict = {
                   "_id": current_id,
                   "concept1": {
@@ -106,11 +71,7 @@ def load_annotations(data_folder):
                     }
                   ]
                 }
-                current_results = []
-                current_results.append(current_dict)
-                ids_included = []
-                ids_included.append(current_id)
-    
-    for x in current_results:
-        yield(x)
+                first_done = True
+                    
+    yield(current_dict)
             
